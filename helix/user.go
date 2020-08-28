7863f619631b5b7e746c5d6271d2a40bfa3ce1ca
gempir <gempir.dev@gmail.com>
2020-08-28 22:56:35 +0200

    update to new helix lib version

--- helix/user.go
+++ helix/user.go
@@ -38,7 +38,7 @@ func NewClient(clientID string, clientSecret string) Client {
 		panic(err)
 	}
 
-	resp, err := client.GetAppAccessToken()
+	resp, err := client.GetAppAccessToken([]string{})
 	if err != nil {
 		panic(err)
 	}
@@ -77,7 +77,7 @@ func (c *Client) StartRefreshTokenRoutine() {
 	ticker := time.NewTicker(24 * time.Hour)
 
 	for range ticker.C {
-		resp, err := c.client.GetAppAccessToken()
+		resp, err := c.client.GetAppAccessToken([]string{})
 		if err != nil {
 			log.Error(err)
 			continue
