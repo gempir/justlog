7e92a8bb48ea55f6362510d8e904503a4267f28c
gempir <gempir.dev@gmail.com>
2020-09-26 09:13:47 +0200

    added DELETE method to reset types

--- api/admin.go
+++ api/admin.go
@@ -20,12 +20,16 @@ func (s *Server) authenticateAdmin(w http.ResponseWriter, r *http.Request) bool 
 	return true
 }
 
+type channelConfigsDeleteRequest struct {
+	MessageTypes bool `json:"messageTypes,omitempty"`
+}
+
 type channelConfigsRequest struct {
 	config.ChannelConfig
 }
 
 func (s *Server) writeConfig(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
+	if r.Method != http.MethodPost && r.Method != http.MethodDelete {
 		http.Error(w, "We'll see, we'll see. The winner gets tea.", http.StatusMethodNotAllowed)
 		return
 	}
@@ -37,16 +41,38 @@ func (s *Server) writeConfig(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var request channelConfigsRequest
+	if r.Method == http.MethodDelete {
+		var request channelConfigsDeleteRequest
 
-	err := json.NewDecoder(r.Body).Decode(&request)
-	if err != nil {
-		http.Error(w, "ANYWAYS: "+err.Error(), http.StatusBadRequest)
+		err := json.NewDecoder(r.Body).Decode(&request)
+		if err != nil {
+			http.Error(w, "ANYWAYS: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if request.MessageTypes {
+			s.cfg.ResetMessageTypes(channelID)
+			s.bot.UpdateMessageTypesToLog()
+			writeJSON("Doubters? Reset "+channelID+" messageTypes", http.StatusOK, w, r)
+			return
+		}
+
+	} else {
+		var request channelConfigsRequest
+
+		err := json.NewDecoder(r.Body).Decode(&request)
+		if err != nil {
+			http.Error(w, "ANYWAYS: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		s.cfg.SetMessageTypes(channelID, request.MessageTypes)
+		s.bot.UpdateMessageTypesToLog()
+
+		writeJSON("Doubters? Updated "+channelID+" messageTypes to "+fmt.Sprintf("%v", request.MessageTypes), http.StatusOK, w, r)
 		return
 	}
 
-	s.cfg.SetMessageTypes(channelID, request.MessageTypes)
-	s.bot.UpdateMessageTypesToLog()
-
-	writeJSON("Doubters? Updated "+channelID+" messageTypes to "+fmt.Sprintf("%v", request.MessageTypes), http.StatusOK, w, r)
+	http.Error(w, "Nothing here", http.StatusBadRequest)
+	return
 }
