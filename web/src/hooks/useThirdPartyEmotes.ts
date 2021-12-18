17aacce50b7bc35e92ff2e3b470d3fb442a5d2f1
gempir <gempir.dev@gmail.com>
2021-12-18 22:03:35 +0100

    add 7tv support

--- web/src/hooks/useThirdPartyEmotes.ts
+++ web/src/hooks/useThirdPartyEmotes.ts
@@ -1,4 +1,6 @@
 import { ThirdPartyEmote } from "../types/ThirdPartyEmote";
+import { use7tvChannelEmotes } from "./use7tvChannelEmotes";
+import { use7tvGlobalEmotes } from "./use7tvGlobalEmotes";
 import { useBttvChannelEmotes } from "./useBttvChannelEmotes";
 import { useBttvGlobalEmotes } from "./useBttvGlobalEmotes";
 import { useFfzChannelEmotes } from "./useFfzChannelEmotes";
@@ -8,8 +10,10 @@ export function useThirdPartyEmotes(channelId: string): Array<ThirdPartyEmote> {
 	const thirdPartyEmotes: Array<ThirdPartyEmote> = [
 		...useBttvChannelEmotes(channelId),
 		...useFfzChannelEmotes(channelId),
+		...use7tvChannelEmotes(channelId),
 		...useBttvGlobalEmotes(),
 		...useFfzGlobalEmotes(),
+		...use7tvGlobalEmotes(),
 	];
 
 	return thirdPartyEmotes;
